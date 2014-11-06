999dfdc7191de5619d204b66617c1178b3de86f0
doc <doc.divxm@gmail.com>
2014-11-06 23:17:58 +0200

    deb: Initial device bringup
    Change-Id: Icb474b1ecaedfde1c2b15200514ef9da05185f60

---
+++ slim.mk
@@ -0,0 +1,31 @@
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH  := 1200
+
+# Release name
+PRODUCT_RELEASE_NAME := deb
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/deb/aosp_deb.mk)
+
+#MMS
+PRODUCT_PACKAGES += \
+    Mms
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := deb
+PRODUCT_NAME := slim_deb
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/deb:4.4.3/KTU84L/1148727:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4.3 KTU84L 1148727 release-keys"
